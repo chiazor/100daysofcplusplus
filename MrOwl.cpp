22324e0c4f89cde2e48462df75e7f800a1dd9773
jennifer kwentoh <kwentohchiazor@gmail.com>
2019-07-17 08:35:45 +0100

    codes for day 1

---
+++ MrOwl.cpp
@@ -0,0 +1,15 @@
+// day1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " _____" << endl;
+	cout << "( @ @ ) MR OWL is watching you code" << endl;
+	cout << "   '  " << endl;
+	
+	return 0;
+}
---
+++ day1.cpp
@@ -0,0 +1,12 @@
+// day1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World My First C++ Program \n"; 
+	
+	return 0;
+}
