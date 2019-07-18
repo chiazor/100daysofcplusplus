3314ba1d9b0624857254af0dc6aad5b81e3f9cc8
jennifer kwentoh <kwentohchiazor@gmail.com>
2019-07-18 11:24:40 +0100

    added exercises 1.0

---
+++ exercise1.0.cpp
@@ -0,0 +1,17 @@
+// day1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " _____    ____    _____   _____   .   ." << endl;
+	cout << "   ||    |        |   |   |   |   |   |"<< endl;
+	cout << "   ||    |----    |   |   |   |    ---|" << endl;
+	cout << "-----     ----    '   '   '   '   ____'" << endl;
+	
+	
+	return 0;
+}
